fcee991b205b9d51fb4a07a7c804d59912aa4da2
Deepak Pujari <spdeepak20@gmail.com>
2016-08-31 17:51:30 +0530

    upload sleep ajax

--- copy.php
+++ copy.php
@@ -7,12 +7,20 @@ function copyfiles($from, $to) {
 
     $buffer_size = 500000; 
     $ret = 0;
+    $fsize = fsize($from);
+    
     $_SESSION[$to] = $ret;
     $fin = fopen($from, "rb");
     $fout = fopen('upload/'.$_POST['title'], "w");
     while(!feof($fin)) {
         session_start();
-        $ret += fwrite($fout, fread($fin, $buffer_size));
+        sleep(1);
+        $cont += fwrite($fout, fread($fin, $buffer_size));
+        if($fsize != null){
+           $ret = = min(100, 100 * $cont / $fsize);
+        }else{
+           $ret = $cont;
+        }
         $_SESSION[$to] = $ret;
         session_write_close();
     }
@@ -28,5 +36,23 @@ function copyfiless($filename, $filesize) {
     echo json_encode('copying ' . $filename);
 }
 
+function fsize($file){
+    $ch = curl_init($file);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    if (preg_match('/Content-Length: (\d+)/', $data, $matches)) {
+        $contentLength = (int)$matches[1];
+    }else{
+        $contentLength = null;
+    }
+    
+    return $contentLength;
+}
 ?>
 
